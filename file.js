e1702fdd2079a3ca8564eee427c02061b089c2af
Rosy <rosydhakal333@gmail.com>
2023-12-18 15:25:41 +1100

    more changes

--- file.js
+++ file.js
@@ -9,21 +9,54 @@ var user = {
     courseList : [],
     buyCourse: function(courseName){
         this.courseList.push(courseName);
+    },
+    getCourseCount: function(){
+        return `${this.firstName} have enrolled you in total of ${this.courseList.length} courses, they are ${this.courseList}, Are you ready to learn?`
     }
 };
+// <============Learning Methods==========>
+var courseList = true;
+console.log(user.firstName);
+
+user.buyCourse(" react JS course");
+user.buyCourse(" Node Js course");
+user.buyCourse(" Angular course");
+
+
+console.log(user.getCourseCount());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //accessing value 
 
-console.log(user.firstName); //printing fist name by accessing with . 'dot' 
-//another way to access the value, not widley used but is possible this way 
-console.log(user["lastName"]);
-//Manipulating value 
-user.loginCount = 44;
-console.log(user.loginCount);
-console.log(user.role);
-console.log(user);
+// console.log(user.firstName); //printing fist name by accessing with . 'dot' 
+// //another way to access the value, not widley used but is possible this way 
+// console.log(user["lastName"]);
+// //Manipulating value 
+// user.loginCount = 44;
+// console.log(user.loginCount);
+// console.log(user.role);
+// console.log(user);
 
-//displaying values in table format
-console.table(user);
+// //displaying values in table format
+// console.table(user);
 
-// <============Learning Methods==========>
+
+
 
