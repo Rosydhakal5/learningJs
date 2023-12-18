b51ddd7797e75f3e09f482ddbcbc70bc2c72c4d4
Rosy <rosydhakal333@gmail.com>
2023-12-18 15:07:01 +1100

    accessing values from obejct

--- file.js
+++ file.js
@@ -1,3 +1,12 @@
+// <============Learning Objects==========>
+
 var user = {
-    
-}
+    firstName : "Rosy",
+    lastName: "Dhakal",
+    role: "Software Developer",
+    loginCount: 32,
+    accountSignedIn : true
+};
+//accessing value 
+
+console.log(user.firstName); //printing fist name by accessing with . 'dot' 
