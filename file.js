369da96081dde1bfc32f91a79684b823590a5fdc
Rosy <rosydhakal333@gmail.com>
2023-12-18 15:08:43 +1100

    accessing values from obejct

--- file.js
+++ file.js
@@ -10,3 +10,5 @@ var user = {
 //accessing value 
 
 console.log(user.firstName); //printing fist name by accessing with . 'dot' 
+//another way to access the value, not widley used but is possible this way 
+console.log(user["lastName"]);
