868ef275e92a694aa9603430b6de48de2f72e571
Rosy <rosydhakal333@gmail.com>
2023-12-18 15:09:58 +1100

    manipulating value

--- file.js
+++ file.js
@@ -12,3 +12,6 @@ var user = {
 console.log(user.firstName); //printing fist name by accessing with . 'dot' 
 //another way to access the value, not widley used but is possible this way 
 console.log(user["lastName"]);
+//Manipulating value 
+user.loginCount = 44;
+console.log(user.loginCount);
